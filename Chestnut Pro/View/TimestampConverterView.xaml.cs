858cf4b14ac3d4a4cc216336119e463c5ad89ae7
LyricYang <983732985@qq.com>
2022-03-19 11:35:24 +0800

    update timestamp

--- Chestnut Pro/View/TimestampConverterView.xaml.cs
+++ Chestnut Pro/View/TimestampConverterView.xaml.cs
@@ -10,6 +10,9 @@
     {
         System.Windows.Threading.DispatcherTimer Timer = new System.Windows.Threading.DispatcherTimer();
 
+        /// <summary>
+        /// The Constructor
+        /// </summary>
         public TimestampConverterView()
         {
             InitializeComponent();
@@ -18,6 +21,11 @@
             Timer.Start();
         }
 
+        /// <summary>
+        /// Time Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TimeClick(object sender, EventArgs e)
         {
             var utc = new DateTimeOffset(DateTime.UtcNow);
@@ -28,6 +36,33 @@
             USTime.Content = $"US(UTC-8:00): {usTime}";
             INTime.Content = $"IN(UTC+5:30): {inTime}";
             CNTime.Content = $"IN(UTC+8:00): {cnTime}";
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UnixToDateTime(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(UnixTimestamp?.Text))
+            {
+                var timestamp = Convert.ToInt64(UnixTimestamp?.Text);
+                var ticks = timestamp * 10000 + 621355968000000000;
+                var datetime = new DateTime(ticks);
+                DateTimeTextBox.Text = datetime.ToLocalTime().ToString();
+            }
+        }
+
+        private void DateTimeToUnix(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(DatePick?.Text) && !string.IsNullOrEmpty(TimePick?.Text))
+            {
+                var timePicker = $"{DatePick.Text} {TimePick.Text}";
+                var datetime = DateTime.Parse(timePicker).ToUniversalTime();
+                var timestamp = (datetime.Ticks - 621355968000000000) / 10000;
+                UnixTimestamp.Text = timestamp.ToString();
+            }
         }
     }
 }
--- Chestnut Pro/ViewModel/NavigationViewModel.cs
+++ Chestnut Pro/ViewModel/NavigationViewModel.cs
@@ -33,15 +33,15 @@
             // get added, removed, or when the whole list is refreshed.
             ObservableCollection<MenuItems> menuItems = new ()
             {
-                new () { MenuName = "Home",                 MenuImage = @"Assets/home.png" },
-                new () { MenuName = "All Tools",            MenuImage = @"Assets/tools-hardware.png" },
-                new () { MenuName = "Number Base",          MenuImage = @"Assets/number-base.png" },
-                new () { MenuName = "GUID Generator",       MenuImage = @"Assets/guid.png" },
-                new () { MenuName = "Base64 Generator",     MenuImage = @"Assets/base64.png" },
-                new () { MenuName = "Timestamp Converter",  MenuImage = @"Assets/timestamp.png" },
-                new () { MenuName = "JSON Formatter",       MenuImage = @"Assets/jsonformat.png" },
-                new () { MenuName = "TSV/CSV Converter",    MenuImage = @"Assets/csv.png" },
-                new () { MenuName = "Color Palette",        MenuImage = @"Assets/palette.png" },
+                new () { MenuName = "Home",              MenuImage = @"Assets/home.png" },
+                new () { MenuName = "All Tools",         MenuImage = @"Assets/tools-hardware.png" },
+                new () { MenuName = "Number Base",       MenuImage = @"Assets/number-base.png" },
+                new () { MenuName = "GUID Generator",    MenuImage = @"Assets/guid.png" },
+                new () { MenuName = "Base64 Generator",  MenuImage = @"Assets/base64.png" },
+                new () { MenuName = "Unix Timestamp",    MenuImage = @"Assets/timestamp.png" },
+                new () { MenuName = "JSON Formatter",    MenuImage = @"Assets/jsonformat.png" },
+                new () { MenuName = "TSV/CSV Converter", MenuImage = @"Assets/csv.png" },
+                new () { MenuName = "Color Palette",     MenuImage = @"Assets/palette.png" },
             };
 
             MenuItemsCollection = new CollectionViewSource { Source = menuItems };
@@ -140,7 +140,7 @@
                 case "JSON Formatter":
                     SelectedViewModel = new JsonFormatterViewModel();
                     break;
-                case "Timestamp Converter":
+                case "Unix Timestamp":
                     SelectedViewModel = new TimestampConverterViewModel();
                     break;
                 default:
