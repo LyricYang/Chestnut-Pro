04a0220456df48e66eb7fd602ecfd80356c9e3ce
LyricYang <983732985@qq.com>
2022-03-19 23:20:53 +0800

    update color palette

---
+++ Chestnut Pro/Model/ColorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Chestnut_Pro.Model
+{
+    public class ColorModel
+    {
+        public string ColorName { get; set; }
+
+        public string ColorHex { get; set; }
+    }
+}
---
+++ Chestnut Pro/Model/HeaderModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Chestnut_Pro.Model
+{
+    public class HeaderModel
+    {
+        public string HeaderName { get; set; }
+    }
+}
