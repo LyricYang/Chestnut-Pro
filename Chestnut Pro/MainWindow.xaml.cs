45e00ffd089fa274c484dcbed820eaffc23993d8
LyricYang <983732985@qq.com>
2022-03-22 21:09:00 +0800

    callopase

--- Chestnut Pro/MainWindow.xaml.cs
+++ Chestnut Pro/MainWindow.xaml.cs
@@ -1,28 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Chestnut_Pro
+﻿namespace Chestnut_Pro
 {
+    using System;
+    using System.Windows;
+    using System.Windows.Threading;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
+        bool hidden = true;
+        double maxWidth = 1320;
+        double minWidth = 1160;
+
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            timer.Tick += TimerTick;
+        }
+
+        /// <summary>
+        /// Timer Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (hidden)
+            {
+                DashboardWindow.Width += 40;
+                if (DashboardWindow.Width >= maxWidth)
+                {
+                    timer.Stop();
+                    hidden = false;
+                }
+            }
+            else
+            {
+                DashboardWindow.Width -= 40;
+                if (DashboardWindow.Width <= minWidth)
+                {
+                    timer.Stop();
+                    hidden = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Collapse Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CollapseClick (object sender, RoutedEventArgs e)
+        {
+            timer.Start();
         }
     }
 }
