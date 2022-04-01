9979b0e4e7b246969b40ec8fdcc7a368dfc733d9
LyricYang <983732985@qq.com>
2022-04-02 00:28:35 +0800

    hash logic

--- Chestnut Pro/View/Generator/HashGeneratorView.xaml.cs
+++ Chestnut Pro/View/Generator/HashGeneratorView.xaml.cs
@@ -1,6 +1,9 @@
 ﻿namespace Chestnut_Pro.View
 {
     using System;
+    using System.Runtime.Intrinsics.Arm;
+    using System.Security.Cryptography;
+    using System.Text;
     using System.Windows;
     using System.Windows.Controls;
 
@@ -9,12 +12,66 @@
     /// </summary>
     public partial class HashGeneratorView : UserControl
     {
+        private readonly MD5 md5 = MD5.Create();
+        private readonly SHA1 sha1 = SHA1.Create();
+        private readonly SHA256 sha256 = SHA256.Create();
+        private readonly SHA512 sha512 = SHA512.Create();
         /// <summary>
         /// The Constructor
         /// </summary>
         public HashGeneratorView()
         {
             InitializeComponent();
+        }
+
+        private void CalculateHash(object sender, RoutedEventArgs e)
+        {
+            var input = SourceInput.Text;
+            var bytesInput = Encoding.UTF8.GetBytes(input);
+            if (OutputTypeSelected.SelectedIndex == 0)
+            {
+                MD5Output.Text = Convert.ToHexString(md5.ComputeHash(bytesInput));
+                SHA1Output.Text = Convert.ToHexString(sha1.ComputeHash(bytesInput));
+                SHA256Output.Text = Convert.ToHexString(sha256.ComputeHash(bytesInput));
+                SHA512Output.Text = Convert.ToHexString(sha512.ComputeHash(bytesInput));
+            }
+            else
+            {
+                MD5Output.Text = Convert.ToBase64String(md5.ComputeHash(bytesInput));
+                SHA1Output.Text = Convert.ToBase64String(sha1.ComputeHash(bytesInput));
+                SHA256Output.Text = Convert.ToBase64String(sha256.ComputeHash(bytesInput));
+                SHA512Output.Text = Convert.ToBase64String(sha512.ComputeHash(bytesInput));
+            }
+        }
+
+        private void CopySource(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(SourceInput.Text);
+        }
+
+        private void CopyMD5(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(MD5Output.Text);
+        }
+
+        private void CopySha1(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(SHA1Output.Text);
+        }
+
+        private void CopySha256(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(SHA256Output.Text);
+        }
+
+        private void CopySha512(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(SHA512Output.Text);
+        }
+
+        private void ClearSource(object sender, RoutedEventArgs e)
+        {
+            SourceInput.Text = string.Empty;
         }
     }
 }
